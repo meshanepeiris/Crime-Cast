8be33b1544478a077045b14a6681a74af77e3b79
Meshane Peiris <meshanepeiris@gmail.com>
2024-09-01 15:06:01 -0400

    Backbone website more developed. News shows on latest news page

--- src/get_headlines.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-table = pd.read_csv('../csv/news.csv', encoding='ISO-8859-1')
-
-columns = table['headline', '']
-
-columns.to_csv('headlines.csv', index=False)
-
-print(table)
